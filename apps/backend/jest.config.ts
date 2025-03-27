f31583ab69ff6ed13be9c862b317b524dfebd774
RohaN <166110329+0to1e@users.noreply.github.com>
2025-03-28 00:10:00 +0545

    inital f/b setup

---
+++ apps/backend/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: '@auth-practice/backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: 'test-output/jest/coverage',
+};
